3733af4d2be40e3e2499cec19d34b4b8c979ce86
JobvAlewijk <extrajvanalewijk@gmail.com>
2024-10-22 17:10:43 +0200

    --vieuw work?

--- gloss-counter/src/Model.hs
+++ gloss-counter/src/Model.hs
@@ -31,7 +31,14 @@ data Score = Sc {currScore :: Int, highScoreName :: String, highScore :: Int}
 
 initialState :: Picture -> GameState
 initialState a = GameState 
-  [((0,0),Empty)] 
+ [
+    ((0, 0), Wall),   ((0, 1), Wall),   ((0, 2), Wall),   ((0, 3), Wall),   ((0, 4), Wall),   ((0, 5), Wall),   ((0, 6), Wall),
+    ((1, 0), Wall),   ((1, 1), Pellet), ((1, 2), Empty),  ((1, 3), Empty),  ((1, 4), Pellet), ((1, 5), Fruit),  ((1, 6), Wall),
+    ((2, 0), Wall),   ((2, 1), Empty),  ((2, 2), Cherry), ((2, 3), Empty),  ((2, 4), Empty),  ((2, 5), Pellet), ((2, 6), Wall),
+    ((3, 0), Wall),   ((3, 1), Empty),  ((3, 2), Empty),  ((3, 3), Empty),  ((3, 4), Pellet), ((3, 5), Empty),  ((3, 6), Wall),
+    ((4, 0), Wall),   ((4, 1), Pellet), ((4, 2), Empty),  ((4, 3), Fruit),  ((4, 4), Cherry), ((4, 5), Empty),  ((4, 6), Wall),
+    ((5, 0), Wall),   ((5, 1), Wall),   ((5, 2), Wall),   ((5, 3), Wall),   ((5, 4), Wall),   ((5, 5), Wall),   ((5, 6), Wall)
+    ]
   (Pac { pacPos = (0,0), pacDir = N, pacDesDir = E, pacLives = 3}) 
   [(Gho {ghostPos = (0,0), ghostDir = N, ghostType = Blinky, ghostState = Normal})] 
   (Sc {currScore = 0, highScoreName = "Bob", highScore = 200}) 
--- gloss-counter/src/View.hs
+++ gloss-counter/src/View.hs
@@ -7,16 +7,34 @@ view = viewPure
 
 
 viewPure :: GameState -> IO Picture
-viewPure gameState@GameState {pacman = (Pac {pacPos = (c,d),pacDir = _, pacDesDir = _, pacLives = _})} = do
-    mappy <- giveBitMap "map"
+viewPure gameState@GameState {grid = grid_ ,pacman = (Pac {pacPos = (c,d),pacDir = _, pacDesDir = _, pacLives = _})} = do
     pacman <- giveBitMap "Pac-Man"
-    let pacManWithMap = Pictures [mappy, translate c d pacman]  -- Adjust (x, y) as needed
-    return pacManWithMap
-      
-      
+    pellet <- giveBitMap "pellet"
+    empty <- giveBitMap "empty"
+    fruit <- giveBitMap "fruit"
+    cherry <- giveBitMap "cherry"
+    wall <- giveBitMap "wall"
+    red <- giveBitMap "red"
+    
 
+    let pacManWithMap = Pictures [ gridToPic grid_ [pellet,empty,cherry,fruit,wall], translate c d pacman]  -- Adjust (x, y) as needed
+    return pacManWithMap
+
+
+gridToPic :: Grid -> [Picture] -> Picture
+gridToPic [((x,y),_)] pel = Pictures [translate (x*20) (y*20) (pel !! 4)]
+gridToPic (((x,y),Pellet):xs) pel = Pictures [translate (x*20) (y*20) (head pel), gridToPic xs pel]
+gridToPic (((x,y),Empty):xs) pel = Pictures [translate (x*20) (y*20) (pel !! 1), gridToPic xs pel]
+gridToPic (((x,y),Cherry):xs) pel = Pictures [translate (x*20) (y*20) (pel !! 2), gridToPic xs pel]
+gridToPic (((x,y),Fruit):xs) pel = Pictures [translate (x*20) (y*20) (pel !! 3), gridToPic xs pel]
+gridToPic (((x,y),Wall):xs) pel = Pictures [translate (x*20) (y*20) (pel !! 4), gridToPic xs pel]
 
 
 giveBitMap :: String -> IO Picture
-giveBitMap "map"     = loadBMP "./Pictures/Map.bmp"
 giveBitMap "Pac-Man" = loadBMP "./Pictures/Pacman-Basic.bmp"
+giveBitMap "pellet" = loadBMP "./Pictures/pellet_tile.bmp"
+giveBitMap "empty"     = loadBMP "./Pictures/empty_tile.bmp"
+giveBitMap "fruit" = loadBMP "./Pictures/cherry_tile.bmp"
+giveBitMap "cherry" = loadBMP "./Pictures/powerpellet_tile.bmp"
+giveBitMap "wall"     = loadBMP "./Pictures/wall_tile.bmp"
+giveBitMap "red" = loadBMP "./Pictures/red_ghost.bmp"
