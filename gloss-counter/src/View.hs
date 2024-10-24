2a7ed8aa1971792d3aa9e3a9b845a6d50571d3b8
JobvAlewijk <extrajvanalewijk@gmail.com>
2024-10-24 15:27:46 +0200

    --txttogrid

--- gloss-counter/src/View.hs
+++ gloss-counter/src/View.hs
@@ -15,22 +15,18 @@ viewPure gameState@GameState {grid = grid_ ,pacman = Pac {pacPos = (c,d)},score 
     power <- giveBitMap "power"
     wall <- giveBitMap "wall"
     red <- giveBitMap "red"
-    
-    
 
-    let pacManWithMap = Pictures [gridToPic grid_ [pellet,empty,cherry,power,wall], translate c d pacman, scoretxt, highscoretxt]  -- Adjust (x, y) as needed
+    -- stringforgrid <- readFile ""
+
+
+    let pacManWithMap = Pictures [Pictures $ map (\sq -> squareToPic sq [pellet,empty,cherry,power,wall]) grid_, translate c d pacman, scoretxt, highscoretxt]  -- Adjust (x, y) as needed
     return pacManWithMap
     where
-        gridpic = map gridToPic 
         scoretxt     = translate (-40) 180 $ scale 0.3 0.3 $ color white $ Text ("Score  " ++ show score_)
         highscoretxt = translate (-40) 140 $ scale 0.15 0.15 $ color white $ Text ("HighScore  " ++ show highScore_)
 
 squareToPic :: Square -> [Picture] -> Picture
-squareToPic ((x,y),field_) pel = Pictures [translate (x*20) (y*20) (pel !! fieldToPic field_)]
-
-gridToPic :: Grid -> [Picture] -> Picture
-gridToPic [] _ = Blank
-gridToPic (((x,y),field_):xs) pel = Pictures [translate (x*20) (y*20) (pel !! fieldToPic field_), gridToPic xs pel]
+squareToPic ((x,y),field_) pics = translate (x*20) (y*20) (pics !! fieldToPic field_)
 
 fieldToPic :: Field -> Int
 fieldToPic Pellet = 0
@@ -51,4 +47,23 @@ giveBitMap "power" = loadBMP "./Pictures/powerpellet_tile.bmp"
 giveBitMap "wall"     = loadBMP "./Pictures/wall_tile.bmp"
 giveBitMap "red" = loadBMP "./Pictures/red_ghost_tile.bmp"
 giveBitMap "blue" = loadBMP "./Pictures/blue_ghost_tile.bmp"
+
+stringToGrid :: String -> Grid
+stringToGrid str = _indexfieldlsit
+ where
+    _indexfieldlsit = map smth $ concat sindexfieldlist
+    sindexfieldlist = map (zip [0..]) findexfieldlist
+    findexfieldlist = map (zip [0..]) fieldlist
+    fieldlist = map (map charToField) linestr
+    linestr = lines str
+
+smth:: (Float,(Float, Field)) -> Square
+smth (y,(x,f)) = ((x,y),f)
+
+charToField :: Char -> Field
+charToField 'X' = Wall
+charToField 'E' = Empty
+charToField '.' = Pellet
+charToField 'S' = Power
+charToField 'C' = Cherry
 
